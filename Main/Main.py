582c469e6cec39a824c6ac529d6166b4565a34c0
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-05-16 16:20:52 -0400

    Update Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -7,10 +7,15 @@ from numpy.f2py.crackfortran import usermodules
 
 import mysql.connector
 
-tabela = pd.read_csv("../Content/e91da71a-5b6e-4a07-8584-707fd264ac45.csv")
-tabela2 = pd.read_excel("../Content/Planilha1.xlsx")
+caminho_csv = "../Content/e91da71a-5b6e-4a07-8584-707fd264ac45.csv"
+caminho_excel = "../Content/Planilha1.xlsx"
 
-print("===================== VENDAS CRM ===================")
-print(tabela.head())
-print("===================== VENDAS SISTEMA ===================")
-print(tabela2.head())
+df_csv = pd.read_csv(caminho_csv)
+df_excel = pd.read_excel(caminho_excel)
+
+df_final = pd.concat([df_csv, df_excel], ignore_index=True)
+df_final.to_excel("arquivo_unificado.xlsx", index=False)
+
+print("Aquivo Excel foi salvo")
+
+

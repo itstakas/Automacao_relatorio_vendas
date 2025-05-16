45fd9a3bebf5a08e4c4294b257c40096ee1bcbf5
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-05-16 14:22:10 -0400

    Update Main.py

--- Main/Main.py
+++ Main/Main.py
@@ -1,1 +1,29 @@
 #CRUDE
+from struct import pack_into
+
+import pandas as pd
+from numpy.f2py.crackfortran import usermodules
+
+print ("Panda esta funcionando")
+
+import mysql.connector
+
+try:
+    conexao = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="1234",
+        database="teste_python"
+    )
+
+    if conexao.is_connected():
+        print("Deu certo a conexão")
+        print("Banco: ", conexao.database)
+
+except mysql.connector.Error as erro:
+    print("Erro ao conectar ", erro)
+
+finally:
+    if 'conexao' in locals() and conexao.is_connected():
+        conexao.close()
+        print("Conexão encerrada")

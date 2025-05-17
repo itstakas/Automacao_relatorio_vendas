659f0ca2e00276d61f345d1068f59e42924d95da
Leonardo Takeshi <takeshileonardo.1@gmail.com>
2025-05-17 08:59:43 -0400

    coenxao com o banco de dados bem sucedida!
    Consegui conectar ao banco de dados, proximo passo é a manipulação desses dados e otimização do sistema

--- Main/Main.py
+++ Main/Main.py
@@ -10,7 +10,7 @@ import mysql.connector
 caminho_csv = "../Content/e91da71a-5b6e-4a07-8584-707fd264ac45.csv"
 caminho_excel = "../Content/Planilha1.xlsx"
 
-df_csv = pd.read_csv(caminho_csv, sep=";")
+df_csv = pd.read_csv(caminho_csv, sep=";") #arquivo csv tem que ter esse "sep=;" ou "sep=." pq se n fica tudo numa linha so
 df_excel = pd.read_excel(caminho_excel)
 
 conexao = mysql.connector.connect(
@@ -20,6 +20,34 @@ conexao = mysql.connector.connect(
     database="bdvendas"
 )
 
+cursor =  conexao.cursor()
 
+comando_sql = """
+CREATE TABLE IF NOT EXISTS relatorio_vendas (
+    unidade VARCHAR(100),
+    contrato VARCHAR(100),
+    nome VARCHAR(255),
+    data_contrato DATE,
+    vendedor VARCHAR(100),
+    regiao VARCHAR(100), 
+    plano VARCHAR(100),
+    dia_pagamento VARCHAR(100),
+    valor_adesao DECIMAL(10,2),
+    data_adesao DATE,
+    concorrente VARCHAR(100),
+    situação VARCHAR(100)    
+);
+"""
+cursor.execute(comando_sql) #Roda o que eu escrevi
+conexao.commit() #Commita/salva no banco de verdade
+
+cursor.execute("DESCRIBE relatorio_vendas") #so pra mostrar a tabela criada mesmo, apagar depois
+for linha in cursor.fetchall():
+    print(linha)
+
+cursor.close() #fecha o cursor pra liberar memoria
+conexao.close() #fecha a conexao com o bd
+
+print("Tabela criada com sucesso") #Confecção de erros
 
 
--- Main/leitura_colunas.py
+++ Main/leitura_colunas.py
@@ -1,3 +1,7 @@
-import panda as pd
+import pandas as pd
 
-df_csv = pd
+df_csv = pd.read_csv("../Content/e91da71a-5b6e-4a07-8584-707fd264ac45.csv", sep=";")
+print("CSV - Colunas: ", df_csv.columns.tolist())
+
+df_excel = pd.read_excel("../Content/Planilha1.xlsx")
+print("Excel - Colunas: ", df_excel.columns.tolist())
